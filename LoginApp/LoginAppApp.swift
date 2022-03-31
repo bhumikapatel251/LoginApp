f2f2288b0c7e3e3d171b890af3fb6c9f51472e88
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-03-31 15:55:24 +0530

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Bhumika Patel on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
